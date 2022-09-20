a1335c70117da404949c1a7d9807aaa251b64eca
unknown <jirochaguerra@gmail.com>
2022-09-20 19:38:48 -0300

    Décimo Update

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -14,9 +14,9 @@ urlpatterns = [
     path('error/', views.error, name="Error"),
     path('chat/', views3.home, name="AppChat"),
     path('AppChat', views3.home, name="AppChat"),
-    #path('A', views3.home, name='home'),
     path('<str:room>/', views3.room, name='room'),
     path('checkview', views3.checkview, name='checkview'),
     path('send', views3.send, name='send'),
     path('getMessages/<str:room>/', views3.getMessages, name='getMessages'),
+
 ]
